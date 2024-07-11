f54643d876f6295bd51680f1a49de436684b75c8
jeongjihyunn <1204jjh@naver.com>
2024-07-11 20:36:38 +0900

    PauseMenu 추가

--- Assets/01.Scripts/PauseMenu.cs
+++ Assets/01.Scripts/PauseMenu.cs
@@ -1,21 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject pauseMenuPanel;
+    public GameObject menuSet;
     private bool isPaused = false;
-
+    private EventSystem eventSystem;
     void Start()
     {
-        // 시작할 때 일시정지 메뉴를 비활성화
-        pauseMenuPanel.SetActive(false); 
+        GameLoad();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        // 서브 메뉴
+        if (Input.GetButtonDown("Cancel"))
         {
             if (isPaused)
             {
@@ -25,26 +27,36 @@ public class PauseMenu : MonoBehaviour
             {
                 PauseGame();
             }
+
         }
     }
 
-    public void ResumeGame()
+    public void GameSave()
     {
-        pauseMenuPanel.SetActive(false);
-        Time.timeScale = 1f; // 게임 시간 다시 진행
-        isPaused = false;
+        //Playerprefs로 저장
+        PlayerPrefs.Save();
+        menuSet.SetActive(false); // 저장 후 게임 재개
     }
+    public void GameLoad()
+    {
 
+    }
+    public void GameExit()
+    {
+        //SceneManager.LoadScene("");
+        Debug.Log("종료");
+    }
     void PauseGame()
     {
-        pauseMenuPanel.SetActive(true);
+        menuSet.SetActive(true);
         Time.timeScale = 0f; // 게임 시간 정지
         isPaused = true;
     }
 
-    public void GoToMainMenu()
+    void ResumeGame()
     {
-        Time.timeScale = 1f; // 게임 시간 다시 진행 (필요시)
-        SceneManager.LoadScene("MainMenu"); // MainMenu라는 씬으로 변경
+        menuSet.SetActive(false);
+        Time.timeScale = 1f; // 게임 시간 다시 진행
+        isPaused = false;
     }
 }
