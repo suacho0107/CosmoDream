17094638a920dd0bb583060943ef17525227e7d2
somrecord <139933035+somrecord@users.noreply.github.com>
2024-07-18 23:19:01 +0900

    클릭, 드래그, 스페이스바 상호작용
    클릭/드래그 드랍이 필요한 오브젝트에 Click and Drag Drop 스크립트를 추가하면 클릭과 드래그를 활성화시킬 수 있습니다.
    
    스페이스 바로 상호작용을 원하는 스크립트에는 Interactable 스크립트를 추가하면 됩니다.
    
    올라갔을 때 커서를 손 모양으로 바꾸고 싶은 오브젝트에는 CursorTrigger 스크립트를 추가하면 됩니다.
    
    클릭
    : 어떤 오브젝트를 클릭했는지 구분하고, 그에 따라 어떤 이벤트가 발생하는지 추가 코딩이 필요합니다.
    
    스페이스바 상호작용
    : 태그 또는 인터페이스로 어떤 오브젝트와 상호작용하였는지 구분하는 코드 추가가 필요합니다.

--- Assets/01.Scripts/Interactable.cs
+++ Assets/01.Scripts/Interactable.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    public void Interact()
+    public virtual void Interact()
     {
-        // 상호작용 로직 추가
-        Debug.Log(gameObject.name + " 오브젝트와 상호작용");
+        // 기본 상호작용 코드
+        Debug.Log("상호작용이 가능한 오브젝트 위에서 스페이스 바 입력");
+    }
+}
+
+public class NPC : Interactable
+{
+    public override void Interact()
+    {
+        // NPC와 상호작용할 경우의..
     }
 }
---
+++ Assets/01.Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    public float movePower = 4.0f;
+    public float interactDistance = 0.6f;
+    private List<Interactable> Interactables = new List<Interactable>();
+
+    void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpacebarInteract();
+        }
+    }
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            moveVelocity = Vector3.left;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            moveVelocity = Vector3.right;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        transform.position += moveVelocity * movePower * Time.deltaTime;
+    }
+
+    void SpacebarInteract()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, interactDistance);
+        Interactables.Clear();
+        foreach (var collider in colliders)
+        {
+            Interactable interactable = collider.GetComponent<Interactable>();
+            if (interactable != null)
+            {
+                Interactables.Add(interactable);
+            }
+        }
+
+        foreach (var interactable in Interactables)
+        {
+            interactable.Interact();
+        }
+    }
+
+    // 디버그 용도, 충돌범위 시각적 표시 (Hierarchy창에서 Player 선택하면 볼 수 있음)
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, interactDistance);
+    }
+}
