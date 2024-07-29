752dca45eff029b8f54e2b478fe9845c685028d1
H <mydiw6610@naver.com>
2024-07-29 09:02:41 +0900

    tmpCommit

--- Assets/01.Scripts/TextFade.cs
+++ Assets/01.Scripts/TextFade.cs
@@ -6,8 +6,6 @@ using UnityEngine.UI;
 public class TextFade : MonoBehaviour
 {
     public CanvasGroup fadeTarget;
-    public float fadeDuration = 1.0f;  // 페이드인 및 페이드아웃의 지속 시간
-
     private FadeController fadeController;
 
     void Start()
@@ -20,9 +18,7 @@ public class TextFade : MonoBehaviour
     IEnumerator FadeInOut()
     {
         yield return StartCoroutine(fadeController.FadeIn(fadeTarget));
-
         yield return new WaitForSeconds(1);
-
         yield return StartCoroutine(fadeController.FadeOut(fadeTarget));
         
     }
