9865b258ba852afe2548f2c1744a9f7881ffb42e
H <mydiw6610@naver.com>
2024-08-10 20:01:33 +0900

    tmpCommit

--- Assets/01.Scripts/RythmGame/Note.cs
+++ Assets/01.Scripts/RythmGame/Note.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Note : MonoBehaviour
 {
     public float noteSpeed = 400;
 
-    // Update is called once per frame
+    Image noteImage;
+
+    private void Start()
+    {
+        noteImage = GetComponent<Image>();
+    }
+
     void Update()
     {
         transform.localPosition += Vector3.right * noteSpeed * Time.deltaTime;
+    }
+
+    public void HideNote()
+    {
+        noteImage.enabled = false;
     }
 }
