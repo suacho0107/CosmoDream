5eca2726292e92f312f5ad9e054b1a7c0b2af69b
H <mydiw6610@naver.com>
2024-10-11 02:00:36 +0900

    tmp

--- Assets/01.Scripts/RythmGame/Note.cs
+++ Assets/01.Scripts/RythmGame/Note.cs
@@ -3,53 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//Note 프리팹에 저장
 public class Note : MonoBehaviour
 {
-    /*
-    public float NoteSpeed;
-    public bool hasStarted;
+    //노트 속도
+    public float noteSpeed = 400f;
+
+    private Image noteImage;
+    private Animator animator;
 
     private void Start()
     {
-        NoteSpeed = NoteSpeed / 60f;
-    }
-
-    private void Update()
-    {
-        if (!hasStarted)
-        {
-            if(Input.anyKeyDown)
-            {
-                hasStarted = true;
-            }
-        }
-        else
-        {
-            transform.position += Vector3.right * NoteSpeed * Time.deltaTime;
-        }
-    }
-    */
-
-    
-    public float noteSpeed = 400;
-
-    Image noteImage;
-
-    private void Start()
-    {
+        animator = GetComponent<Animator>();
         noteImage = GetComponent<Image>();
     }
 
     void Update()
     {
+        //노트 이동
         transform.localPosition -= Vector3.right * noteSpeed * Time.deltaTime;
     }
 
     public void HideNote()
     {
-        //Destroy(this);
-        noteImage.enabled = false;
-        Debug.Log("제발제발제발");
+        StartCoroutine(JudgementNote());
     }
-    
+
+    IEnumerator JudgementNote()
+    {
+        //애니메이션 재생
+        animator.SetBool("isJud", true);
+        yield return new WaitForSeconds(0.5f);
+        noteImage.enabled = false;
+    }
 }
---
+++ Assets/01.Scripts/Start/StartController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartController : MonoBehaviour
+{
+    public Button StartBtn;
+    public Button ContinueBtn;
+    public Button ExitBtn;
+
+    public CanvasGroup fade;
+
+    private DataController datacontroller = new DataController();
+    private FadeController fadecontroller = new FadeController();
+
+    private void Start()
+    {
+        datacontroller = FindObjectOfType<DataController>();
+        fadecontroller = FindObjectOfType<FadeController>();
+
+        StartBtn.onClick.AddListener(GameStart);
+        ContinueBtn.onClick.AddListener(GameContinue);
+        ExitBtn.onClick.AddListener(ExitGame);
+    }
+
+    private void Update()
+    {
+        //if jsonFile존재
+        if (datacontroller.isSave())
+        {
+            ContinueBtn.interactable = true;
+        }
+        else
+        {
+            ContinueBtn.interactable = false;
+        }
+        
+    }
+
+    private void GameStart()
+    {
+        datacontroller.newGameData();
+        StartCoroutine(StartGame());
+    }
+
+    private void GameContinue()
+    {
+        datacontroller.LoadGameData();
+        StartCoroutine(ContinueGame());
+    }
+
+    private void ExitGame()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
+    }
+
+    private IEnumerator StartGame()
+    {
+        //Fade
+        yield return StartCoroutine(fadecontroller.FadeIn(fade));
+        yield return new WaitForSeconds(1f);
+        //이름입력창으로 이동
+        Debug.Log("게임 시작됨");
+    }
+
+    private IEnumerator ContinueGame()
+    {
+        //Fade
+        yield return StartCoroutine(fadecontroller.FadeIn(fade));
+        yield return new WaitForSeconds(1f);
+        //마지막 맵에서 시작
+    }
+
+}
