519b6975f4989f3d4dc2681f70ada995094aefe1
jeongjihyunn <1204jjh@naver.com>
2024-12-02 23:28:18 +0900

    1202-3

--- Assets/01.Scripts/Data/PlayerPosData.cs
+++ Assets/01.Scripts/Data/PlayerPosData.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerPosData
 {
-    public static Vector3 pos;
+    public static Vector3 pos = new Vector3(-7.7f, -1.7f, 0); 
 
     //사용법
     //PlayerPosData.posX=(플레이어).transform.position.x;
